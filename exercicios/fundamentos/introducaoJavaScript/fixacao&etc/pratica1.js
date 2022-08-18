49270cefa035e7dbec6df287208a3d1a2ba5b467
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-18 16:06:01 -0300

    primeira pratica js

---
+++ exercicios/fundamentos/introducaoJavaScript/fixacao&etc/pratica1.js
@@ -0,0 +1,11 @@
+const myName = "Marllon";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1991;
+
+console.log(myName);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "qualquer";
+console.log(birthCity);
