6ead230a2eb294c5b369e78c00421cd395ec7470
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-19 18:15:59 -0300

    exercicio01bl4d1 done

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-01/exercicio01bl4d1.js
@@ -0,0 +1,15 @@
+const a = 10;
+const b = 5;
+
+const progsoma = a + b;
+const progsub = a - b ;
+const progmult = a * b ;
+const progdiv = a / b ;
+const progmod = a % b ;
+
+console.log("O resultado da divisão entre A e B é :" + progdiv)
+console.log("O resto da divisão entre A e B é :" + progmod)
+console.log("O resultado da multiplicação entre A e B é :" + progmult)
+console.log("O resultado da soma entre A e B é :" + progsoma)
+console.log("O resultado da subtração entre A e B é :" + progsub)
+
