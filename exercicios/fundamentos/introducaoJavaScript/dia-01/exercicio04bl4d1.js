998d5004bd9a314fc1d3957ca4d7e8c779798ecf
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-19 18:47:06 -0300

    exercicio04bl4d1 done

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-01/exercicio04bl4d1.js
@@ -0,0 +1,11 @@
+let valor = 0;
+
+if (valor > 0) {
+    console.log("positive")
+} else if (valor < 0) {
+    console.log("negative")
+} else if (valor == 0) {
+    console.log("Zero")
+}else {
+    console.log("insira um valor válido")
+}
