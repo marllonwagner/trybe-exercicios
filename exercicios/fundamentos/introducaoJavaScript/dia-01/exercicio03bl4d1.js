0c02190da0780b5deda2b27ebc5147942bebece6
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-19 18:37:42 -0300

    exercicio03bl4d1 done

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-01/exercicio03bl4d1.js
@@ -0,0 +1,13 @@
+const num1 = 0;
+const num2 = 100;
+const num3 = 15
+
+
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1)
+} else if (num2 > num1 && num2 > num3){
+    console.log(num2)
+}else {
+    console.log(num3)
+};
