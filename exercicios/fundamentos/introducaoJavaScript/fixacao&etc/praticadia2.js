a224c55de044ee63b9c8aee71a52ff4495f1a179
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-20 15:51:50 -0300

    praticadia2 done

---
+++ exercicios/fundamentos/introducaoJavaScript/fixacao&etc/praticadia2.js
@@ -0,0 +1,50 @@
+/* pratica 1 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1] ;
+
+console.log(menuServices);
+*/
+
+/* pratica 2 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio") ;
+
+console.log(indexOfPortfolio);
+
+*/
+
+/* Pratica 3 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+console.log(menu);
+
+*/
+
+/* pratica 4 
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index = index + 1) {
+    console.log(groceryList[index]);
+}
+*/
+
+/* pratica 5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nomes of names) {
+  
+    console.log(nomes)
+}
+*/
+
+
+let counter = 0;
+
+while(counter !== 5) {
+  counter += 1;
+}
