65e86e5f80b2cf79b6fcc3e9214d6e7e359139db
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-19 21:32:46 -0300

    exercicio06bl4d1 done

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-01/exercicio06bl4d1.js
@@ -0,0 +1,26 @@
+let nomepeca = "PEaO".toLowerCase();
+
+switch (nomepeca) {
+
+case "peao":
+case "torre":
+console.log("frente")
+
+break;
+
+case "rei":
+case "rainha" :   
+    console.log("todas as direções")
+
+break
+
+case "cavalo":
+    console.log("movimento em L")
+
+break
+
+case "bispo":
+    console.log("diagonais")
+
+
+}
