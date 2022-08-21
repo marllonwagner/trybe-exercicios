57bba2c27003ca265eb162eed55a59d9bcabdcbb
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-21 18:16:47 -0300

    exercicio02bl4dia2 done ???

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-02/exercicio06bl4dia2 .js
@@ -0,0 +1,27 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+numbers.sort((a, b) => a - b);
+
+let numbersImpars = []
+
+for (index  = 0 ; index < numbers.length ; index += 1){  
+   if( numbers[index] % 2 !== 0){
+    numbersImpars.push(numbers[index] % 2 !==0)
+   } else if ( numbers[index] % 2 == 0) {
+   // nada //
+   }else {
+    console.log("nenhum par encontrado")
+   }
+}
+console.log("existem : " + numbersImpars.length +  " " + "numeros impares nessa array")
+
+
+
+
+
+/*for(let index2 = numbers.length -1 ;index2 > 0 ;index2 -= 1)
+    console.log()*/
+
+   /*if(numbers[index] > numbers[index2]) {
+    console.log(numbers[index])
+}*/
