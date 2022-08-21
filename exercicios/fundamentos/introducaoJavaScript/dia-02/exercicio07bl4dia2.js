69eea460669725fa4d03c21e99e66c2a3681a4cf
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-21 18:29:22 -0300

    exercicio07bl4dia2 done ???

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-02/exercicio07bl4dia2.js
@@ -0,0 +1,21 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+numbers.sort((a, b) => a - b);
+
+
+
+for (index  = 0 ; index <= 0 ; index += 1){  
+    console.log(numbers[index])
+}
+
+
+
+
+
+
+/*for(let index2 = numbers.length -1 ;index2 > 0 ;index2 -= 1)
+    console.log()*/
+
+   /*if(numbers[index] > numbers[index2]) {
+    console.log(numbers[index])
+}*/
