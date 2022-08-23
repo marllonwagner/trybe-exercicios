f462fe3e75e90675b6f5383c79de688884b4dcee
Marllon Wagner <marllonwagner91@gmail.com>
2022-08-23 11:23:31 -0300

    exercicio02bl4d3 done

---
+++ exercicios/fundamentos/introducaoJavaScript/dia-03/exercicio02bl4d3.js
@@ -0,0 +1,29 @@
+let word = 'tryber';
+
+let trocaLetra = " ";
+
+//for (let index = 0; index < word.length; index += 1) {
+for (let index2 = word.length - 1 ; index2 >= 0; index2 -= 1) {
+    trocaLetra += word[index2]
+    }
+console.log(trocaLetra);
+
+
+   
+
+
+
+
+
+
+
+// for (let index2 = word.length ; index2 >= 0; index2 -= 1) {
+//     console.log(word[index2]);
+    
+// }
+// if (word[index] !== word[index2]) {
+//     word[index +1] = word[index2 -1]
+// } 
+
+// console.log(word);
+//trocaLetra += word[index]
